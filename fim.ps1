00b53276bc2a7590d7be117fda5b25d9dd31b838
James Baumhardt <112421154+James1950@users.noreply.github.com>
2024-06-20 13:02:12 -0400

    Add files via upload

---
+++ fim.ps1
@@ -0,0 +1,23 @@
+﻿
+
+
+
+Write-Host "What would you like to do?"
+Write-Host "A) Collect New Baseline?"
+Write-Host "B) Begin monitoring files with saved Baseline?"
+
+$response = Read-Host -Prompt "Please enter 'A' or 'B'"
+
+Function Calculate-File-Hash($filepath) {
+    $filehash = Get-FileHash -Path $filepath -Algorithm SHA512
+    return $filehash
+}
+
+if ($response -eq "A".ToUpper()) {
+    #Calculate Hash from the target files and store in baseline.txt
+    Write-Host "Calculate hashes, make new baseline.txt" -ForegroundColor Cyan
+    }
+elseif ($response -eq "B".ToUpper()) {
+    #Begin monitoring files with saved baseline
+    Write-Host "Read exsting baseline.txt, start monitoring files." -ForegroundColor Yellow
+    }
